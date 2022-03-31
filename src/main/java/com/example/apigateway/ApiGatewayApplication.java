363d80adc0bf775e3b36ebd75a0b125ca4774355
GGmaz <kajtes.nikola@gmail.com>
2022-03-31 10:36:24 +0200

    improvement: add route locator to gateway

--- src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,5 +13,15 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator userServiceRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+					.route(r -> r.path("/clients/**")
+							.uri("http://localhost:8000/clients"))
+//					.route(r -> r.path("/drugiendpoint/**")
+//					.uri("urlzadrugimikroservis"))
+				.build();
+
+	}
 
 }
