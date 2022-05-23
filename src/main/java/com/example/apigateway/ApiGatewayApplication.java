9fc4847bdbe5e5f1870099fddf095e7d62bb4366
Dionizij Malacko <57137277+DionizijMalacko@users.noreply.github.com>
2022-05-23 11:46:17 +0200

    Feat/docker compose (#6)
    * feat: add docker file
    
    * fix/change router paths

--- src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.example.apigateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -9,6 +10,18 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
+	@Value("${user.url}")
+	private String userUrl;
+
+	@Value("${hotel.url}")
+	private String hotelUrl;
+
+	@Value("${cart.url}")
+	private String cartUrl;
+
+	@Value("${email.url}")
+	private String emailUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
@@ -17,7 +30,7 @@ public class ApiGatewayApplication {
 	public RouteLocator userServiceRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/clients/**")
-						.uri("http://localhost:8000/clients"))
+						.uri(userUrl + "clients"))
 						.build();
 	}
 
@@ -25,11 +38,11 @@ public class ApiGatewayApplication {
 	public RouteLocator hotelServiceRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/hotels/**")
-						.uri("http://localhost:8200/hotels"))
+						.uri(hotelUrl + "hotels"))
 				.route(r -> r.path("/cities/**")
-						.uri("http://localhost:8200/cities"))
+						.uri(hotelUrl+"cities"))
 				.route(r -> r.path("/destinations/**")
-						.uri("http://localhost:8200/destinations"))
+						.uri(hotelUrl+"destinations"))
 				.build();
 	}
 
@@ -37,11 +50,11 @@ public class ApiGatewayApplication {
 	public RouteLocator cartServiceRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/carts/**")
-						.uri("http://localhost:8100/carts"))
+						.uri(cartUrl+"carts"))
 				.route(r -> r.path("/reservations/**")
-						.uri("http://localhost:8100/reservations"))
+						.uri(cartUrl+"reservations"))
 				.route(r -> r.path("/invoices/**")
-						.uri("http://localhost:8100/invoices"))
+						.uri(cartUrl+"invoices"))
 				.build();
 	}
 
@@ -49,7 +62,7 @@ public class ApiGatewayApplication {
 	public RouteLocator emailServiceRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/email/**")
-						.uri("http://localhost:8300/sendConfirmationMail"))
+						.uri(emailUrl+"sendConfirmationMail"))
 				.build();
 	}
 }
